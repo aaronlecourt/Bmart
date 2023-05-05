e351f09c877f4143e93b7b7855753c07755008b3
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-05-05 09:23:28 +0800

    clutch commit

--- bmart/app/Http/Middleware/UserAccess.php
+++ bmart/app/Http/Middleware/UserAccess.php
@@ -13,20 +13,49 @@ class UserAccess
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $isVendor)
-    {
-        // if(auth()->user()->isVendor == $isVendor){
-        //     return $next($request);
-        // }
-        // return response()->redirectToRoute('vendor.home');
+//     public function handle(Request $request, Closure $next, $isVendor)
+//     {
+//         // if(auth()->user()->isVendor == $isVendor){
+//         //     return $next($request);
+//         // }
+//         // return response()->redirectToRoute('vendor.home');
 
-        if(auth()->user()->isVendor == $isVendor){
-            return $next($request)
-            //prevent the site from cache-ing and redirects to login when user tries to go back after log out
+//         if(auth()->user()->isVendor =='buyer'){
+//             return $next($request)
+//             //prevent the site from cache-ing and redirects to login when user tries to go back after log out
+//             ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+//             ->header('Pragma', 'no-cache')
+//             ->header('Expires', '0');
+//         }else if(auth()->user()->isVendor == 'vendor'){
+//             return redirect()->route('vendor.home')
+//             //prevent the site from cache-ing and redirects to login when user tries to go back after log out
+//             ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+//             ->header('Pragma', 'no-cache')
+//             ->header('Expires', '0');
+//         }
+//         return back();
+//     }
+// }
+public function handle(Request $request, Closure $next, $isVendor)
+{
+    if(auth()->user()->isVendor == $isVendor){
+        return $next($request);
+    }
+
+    if(auth()->user()->isVendor == 'buyer'){
+        return redirect()->route('home')
             ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('Expires', '0');
-        }
-        return back();
     }
+
+    if(auth()->user()->isVendor == 'vendor'){
+        return redirect()->route('vendor.home')
+            ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
+    }
+
+    return back();
+}
 }
