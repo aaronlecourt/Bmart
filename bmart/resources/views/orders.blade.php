82cf26259be6bbe1b2ff7e7fb7788b217b73209c
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-05-09 11:03:52 +0800

    commit order, nxt ord conf n ord cancel

--- bmart/resources/views/orders.blade.php
+++ bmart/resources/views/orders.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'All Orders')
 @section('content')
 <div id="section-cont" class="p-5">
     <h3 style="font-weight:600; text-align:center;">All Orders</h3>
@@ -9,12 +9,12 @@
             <tr>
                 <th>ID</th>
                 <th>Name</th>
-                <th>Address</th>
+                <th>Shipping Address</th>
                 {{-- <th>City</th>
                 <th>Country</th>
-                <th>Postal Code</th>
+                <th>Postal Code</th> --}}
                 <th>Phone</th>
-                <th>Email</th> --}}
+                <th>Email</th>
                 <th>Total Price</th>
                 <th>Status</th>
                 <th>Order Date:</th>
@@ -27,12 +27,9 @@
                 <tr>
                     <td>{{ $order->id }}</td>
                     <td>{{ $order->name }}</td>
-                    <td>{{ $order->address }}</td>
-                    {{-- <td>{{ $order->city }}</td>
-                    <td>{{ $order->country }}</td>
-                    <td>{{ $order->postalcode }}</td>
+                    <td>{{ $order->address }}, {{ $order->city }}, {{ $order->postalcode }}, {{ $order->country }} </td>
                     <td>{{ $order->phone }}</td>
-                    <td>{{ $order->email }}</td> --}}
+                    <td>{{ $order->email }}</td>
                     <td>P{{ $order->total_price }}</td>
                     <td>
                         <span class="bg bg-warning rounded-pill px-2">
